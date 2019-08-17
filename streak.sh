92b66d2f046ba07c0467d74dec8bac6fbefa69d9
Kofi Addo-Nyarko <kofidazy@gmail.com>
2019-08-17 16:03:29 +0000

    Update streak.sh

--- streak.sh
+++ streak.sh
@@ -2,6 +2,8 @@
 database="mongo"
 declare -a props
 endofprops=false
+property_regex='^/s.+;$'
+
 read -p "Name your model example (Student): " name
 
 until [ "$endofprops" = true ]
@@ -15,23 +17,48 @@ do
   fi
 done
 
-for i in "${props[@]}"; do echo "$i"; done
+echo "Job complete!!!!"
 cat > $name.java << ENDOFFILE
-import org.springframework.stereotype.Component;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 public class $name{
-  $(
-    SAVEIFS=$IFS  
-    IFS=$',' 
-    names=($props)
-    IFS=$SAVEIFS
 
-    for (( i=0; i<${#names[@]}; i++ ))
-    do
+$(
+  SAVEIFS=$IFS  
+  IFS=$',' 
+  names=($props)
+  IFS=$SAVEIFS
+
+  for (( i=0; i<${#names[@]}; i++ ))
+  do
+      type=$(echo ${names[$i]} | cut -d' ' -f 1)
+      variable=$(echo ${names[$i]} | cut -d' ' -f 2)
+
+      if [ "$variable" = "id" ]
+      then 
+        echo "@Id private ${names[$i]};"
+      else
         echo "private ${names[$i]};"
-    done
-  )
+      fi
+  done
+  
+  for (( i=0; i<${#names[@]}; i++ ))
+  do
+      type=$(echo ${names[$i]} | cut -d' ' -f 1)
+      variable=$(echo ${names[$i]} | cut -d' ' -f 2)
+      
+
+      echo "private $type get$variable() { return $variable; }"
+      echo "private void set$variable(${names[$i]}) { this.$variable = $variable; }"
+
+  done
+
+)
 
 }
 ENDOFFILE
